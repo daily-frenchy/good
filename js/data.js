2309b88afcdf05e9f3d2c3c22a71866a76706d40
Karatsu-life <169158813+Karatsu-life@users.noreply.github.com>
2024-05-13 14:30:08 +0200

    Update and rename data.js.txt to data.js

---
+++ js/data.js
@@ -0,0 +1,16 @@
+// Données pour les articles
+const articlesData = [
+  {
+    id: 'article1',
+    title: 'Article 1',
+    image: 'assets/images/photo-1.jpg',
+    description: 'Description de l\'article 1.'
+  },
+  {
+    id: 'article2',
+    title: 'Article 2',
+    image: 'assets/images/photo-2.jpg',
+    description: 'Description de l\'article 2.'
+  },
+  // Ajoutez plus d'articles ici
+];
