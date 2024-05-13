2fe06087317f787b20d5077a9c8017f640ae22d5
Karatsu-life <169158813+Karatsu-life@users.noreply.github.com>
2024-05-13 14:32:15 +0200

    Update data.js

--- js/data.js
+++ js/data.js
@@ -14,3 +14,23 @@ const articlesData = [
   },
   // Ajoutez plus d'articles ici
 ];
+
+
+// Données pour la photo centrale
+const photoCentrale = {
+  image: 'assets/images/photo-centrale.jpg',
+  alt: 'Description de la photo centrale'
+};
+
+// Données pour le photorama
+const photorama = [
+  {
+    image: 'assets/images/photo-1.jpg',
+    alt: 'Description de la photo 1'
+  },
+  {
+    image: 'assets/images/photo-2.jpg',
+    alt: 'Description de la photo 2'
+  },
+  // Ajoutez plus de photos ici
+];
